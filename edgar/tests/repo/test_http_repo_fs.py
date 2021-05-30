da6a8bd61e0869da7a6dbc1f314e17ca46605bf6
clicktripz <l87dg5f1!>
2021-05-29 22:59:25 -0700

    Added testing

--- edgar/tests/repo/test_http_repo_fs.py
+++ edgar/tests/repo/test_http_repo_fs.py
@@ -1,19 +1,26 @@
-import pytest
+import pytest, unittest
 
 from edgar.utils.repo.http_repo_fs import HttpRepoFS
+from edgar.utils.repo.http_repo_object import HttpRepoObject
 from edgar.utils.repo.http_tools import get_index_macro
 from edgar.utils.repo.repo_fs import RepoFormat, RepoFormatter, RepoObject
-from edgar.utils.date.date_utils import DatePeriodType
+from edgar.utils.date.date_utils import DatePeriodType, Date
 
 class TestHttpRepoFS:
-    def setUp(self):
+    def setup_method(self) -> None:
         self.__formatter = RepoFormatter(RepoFormat({
             DatePeriodType.DAY: 'master{y}{m:02}{d:02}.idx', DatePeriodType.QUARTER: 'master.idx'},
             ['{index}', '{y}', 'QTR{q}']))
         self.__formatter['index'] = get_index_macro()
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.__formatter
 
-    def test_find_object(self) -> None:
-        pass
+    @pytest.mark.parametrize("period_type, date_str, expected", [
+        (DatePeriodType.DAY,     '2020-03-17', 'https://www.sec.gov/Archives/edgar/daily-index/2020/QTR1/master20200317.idx'),
+        (DatePeriodType.QUARTER, '2021-06-17', 'https://www.sec.gov/Archives/edgar/full-index/2021/QTR2/master.idx')
+    ])
+    def test_find_object(self, period_type, date_str, expected) -> None:
+        repo: HttpRepoFS = HttpRepoFS('https://www.sec.gov/Archives/edgar/', self.__formatter)
+        obj: HttpRepoObject = repo.find(period_type, Date(date_str))
+        assert obj.as_uri() == expected
